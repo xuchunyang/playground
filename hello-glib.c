9a7074ec33d4ea6837cc3fbccdf6756b3273e0ff
Chunyang Xu <mail@xuchunyang.me>
2017-10-23 22:07:38 +0800

    * hello-glib.c: New file.

---
+++ hello-glib.c
@@ -0,0 +1,15 @@
+#include <glib.h>
+#include <stdio.h>
+
+int
+main (int argc, char *argv[])
+{
+  GList* list = NULL;
+  list = g_list_append (list, "Hello world!");
+  printf ("The first item is '%s'\n", (g_list_first (list))->data);
+  return 0;
+}
+
+/* Local Variables: */
+/* compile-command: "cc `pkg-config --cflags --libs glib-2.0` -o hello-glib hello-glib.c" */
+/* End: */
